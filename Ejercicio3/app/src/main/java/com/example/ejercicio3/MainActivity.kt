233108476ebabe50cf69a670c952fc242b5da61b
Evert Moreno Serrate <evert23122004@gmail.com>
2025-11-19 16:37:19 -0400

    Feat: uso de la Screen creada en MainActivity

--- Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
+++ Ejercicio3/app/src/main/java/com/example/ejercicio3/MainActivity.kt
@@ -12,36 +12,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ejercicio3.ui.theme.Ejercicio3Theme
+import com.example.ejercicio3.ui.Ejercicio3Screen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Ejercicio3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Ejercicio3Screen()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Ejercicio3Theme {
-        Greeting("Android")
+        Ejercicio3Screen()
     }
 }
